75b7534f2486e73fb5c10371ac9af1c5a25989c6
rafic1 <rafic@victorfontes.com>
2021-08-11 21:45:17 -0300

    fixed ALLOWED_HOSTS cast

--- emailsender/settings.py
+++ emailsender/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOST', default='', cast=Csv)
+ALLOWED_HOSTS = config('ALLOWED_HOST', default='', cast=Csv())
 
 
 # Application definition
